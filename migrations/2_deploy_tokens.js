e540c8ed08b45e92946bf106264afbc8db6eb5c5
jimao <1982775886@qq.com>
2021-05-17 11:14:36 +0800

    修改供应量

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,5 +1,5 @@
 const EIP20 = artifacts.require('./EIP20.sol');
 
 module.exports = (deployer) => {
-  deployer.deploy(EIP20, 100000000, 'Jimao Token', 8, 'JMAO');
+  deployer.deploy(EIP20, 10000000000000000, 'Jimao Token', 8, 'JMAO');
 };
